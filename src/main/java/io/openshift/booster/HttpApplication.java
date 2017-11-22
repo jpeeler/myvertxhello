f26301a2ee9b76637e7d23b7a2dd13755c3762c3
Jeff Peeler <jpeeler@redhat.com>
2017-11-22 18:02:59 +0000

    modify text string

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  protected static final String template = "Hello, %s!";
+  protected static final String template = "Hello from (you better believe it too) Che, %s!";
 
   @Override
   public void start(Future<Void> future) {
